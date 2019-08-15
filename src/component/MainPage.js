9401ce26946c7747119bac862d751e11c9ec9d4b
Diuer <diuer71@gmail.com>
2019-08-16 01:43:41 +0800

    Initial commit

---
+++ src/component/MainPage.js
@@ -0,0 +1,28 @@
+import * as React from 'react'
+import './MainPage.scss';
+import CurrentTask from './CurrentTask';
+import TaskTimer from './TaskTimer';
+
+class MainPage extends React.Component{
+    constructor(){
+        super()
+    }
+
+    render(){
+        const {tasks, isTaskPlaying, isTaskWorking, handleTasks, handleTaskPlaying} = this.props
+        
+        return (
+            (tasks.length > 0) ? (
+            <div className='main-page'>
+                <CurrentTask firstTask={tasks[0]} isTaskWorking={isTaskWorking}/>
+                <TaskTimer isTaskPlaying={isTaskPlaying} isTaskWorking={isTaskWorking} nowTimePlace={tasks[0].nowTimePlace} handleTasks={handleTasks} handleTaskPlaying={handleTaskPlaying}/>
+                <footer>PODOMORO</footer>
+            </div>
+            ):(
+                <div className='main-page'></div>
+            )
+        )
+    }
+}
+
+export default MainPage
