1b9c7eab01a9e238a190eb1ca2fd1bfdb4044ad3
Yi-Ting, Zeng <33318685+Diuer@users.noreply.github.com>
2019-08-16 01:48:53 +0800

    init deploy

---
+++ precache-manifest.35d786ca7f32b70b9a08c2570c928345.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0da059a2629efc1717d7e168ca31f1e5",
+    "url": "/pomodoro/index.html"
+  },
+  {
+    "revision": "0013b9a8b1c076474184",
+    "url": "/pomodoro/static/css/main.9a0bcfca.chunk.css"
+  },
+  {
+    "revision": "3a5ab3cb1a42f0221d71",
+    "url": "/pomodoro/static/js/2.d5bb79c6.chunk.js"
+  },
+  {
+    "revision": "0013b9a8b1c076474184",
+    "url": "/pomodoro/static/js/main.78f11e59.chunk.js"
+  },
+  {
+    "revision": "8d8194bde761299ff809",
+    "url": "/pomodoro/static/js/runtime~main.5f1f8454.js"
+  }
+]);
